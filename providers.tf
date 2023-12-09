0bb14787dab4df9325659bd5067d11e6b52b0e18
chathra <chathraserasinghe89@gmail.com>
2023-12-09 14:44:38 +0800

    Using Terraform s3 backend for terraform state

--- providers.tf
+++ providers.tf
@@ -1,5 +1,10 @@
 terraform {
-
+  backend "s3" {
+    bucket = "chathra-tfstate-bkt" #change this
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+    #dynamodb_table = "my-lock-table" # optional (Only if you created the DynamoDB table in step 4) 
+  }
 }
 
 provider "aws" {
